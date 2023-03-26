12af0b23748cd9bbbe6b5b883e63b872b8096630
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-26 16:43:26 -0500

    Implemented compute button and shades grids

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import { BaseColorControls } from './Components/BaseColorControls';
 import { BaseColorsPreview } from './Components/BaseColorsPreview';
+import { ShadesGridPreview } from './Components/ShadesGridPreview';
+import { computeShadesGrid } from './utils/computeShadesGrid';
+import { getEmptyShadesGrid } from './utils/getEmptyShadesGrid';
 
 const initialColors = [
   '#ffffff', //white
@@ -14,7 +17,11 @@ const initialColors = [
   '#000000', //black
 ];
 
+const emptyShades = getEmptyShadesGrid(initialColors.length);
+
 function App() {
+  const [darkerShades, setDarkerShades] = useState([...emptyShades]);
+  const [lighterShades, setLighterShades] = useState([...emptyShades]);
   const [baseColors, setBaseColors] = useState(initialColors);
 
   const handleColorChange = (index: number, color: string) => {
@@ -23,13 +30,37 @@ function App() {
     setBaseColors(newColors);
   };
 
+  const handleComputeShades = () => {
+    const { darkerGridColors, lighterGridColors } = computeShadesGrid(
+      baseColors,
+      6
+    );
+    setDarkerShades(darkerGridColors);
+    setLighterShades(lighterGridColors);
+  };
+
   return (
     <div className="App">
       <BaseColorControls
         initialColors={initialColors}
         onColorChange={handleColorChange}
       />
-      <BaseColorsPreview colors={baseColors} />
+      <div>
+        <div className="palette">
+          <ShadesGridPreview
+            colors={lighterShades}
+            columnCount={initialColors.length}
+          />
+          <BaseColorsPreview colors={baseColors} />
+          <ShadesGridPreview
+            colors={darkerShades}
+            columnCount={initialColors.length}
+          />
+        </div>
+        <button className="compute-btn" onClick={handleComputeShades}>
+          Compute Palette
+        </button>
+      </div>
     </div>
   );
 }
