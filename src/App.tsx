e29989bd869fc18c9174ab5cc459f9838b406108
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-26 15:21:25 -0500

    Implemented color update for BaseColors controls and preview components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import './App.css';
-import { HslColorControl } from './Components/HslColorControl';
+import { BaseColorControls } from './Components/BaseColorControls';
+import { BaseColorsPreview } from './Components/BaseColorsPreview';
+
+const initialColors = [
+  '#ffffff', //white
+  '#ff0000', //red
+  '#ffff00', //yellow
+  '#00ff00', //green
+  '#00ffff', //cyan
+  '#0000ff', //blue
+  '#ff00ff', //magenta
+  '#000000', //black
+];
 
 function App() {
+  const [baseColors, setBaseColors] = useState(initialColors);
+
+  const handleColorChange = (index: number, color: string) => {
+    const newColors = [...baseColors];
+    newColors.splice(index, 1, color);
+    setBaseColors(newColors);
+  };
+
   return (
     <div className="App">
-      <HslColorControl initialColor="#00ee55" onColorChange={() => {}} />
+      <BaseColorControls
+        initialColors={initialColors}
+        onColorChange={handleColorChange}
+      />
+      <BaseColorsPreview colors={baseColors} />
     </div>
   );
 }
