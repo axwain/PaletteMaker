d1250d0a3b6203e0250b5308a195a073f4ff6eb9
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-26 16:44:59 -0500

    Refactored magic numbers into constants

--- src/utils/computeShades.ts
+++ src/utils/computeShades.ts
@@ -1,5 +1,9 @@
 import { darken, lighten, parseToHsl } from 'polished';
 
+const transparentColor = 'rgb(0, 0, 0, 0)';
+const higherLightnessThreshold = 0.8;
+const lowerLightnessThreshold = 0.2;
+
 export function computeShades(
   color: string,
   numberOfShades: number,
@@ -23,12 +27,12 @@ export function computeShades(
     lighterShades.length + darkerShades.length < numberOfShades &&
     count < maxShades
   ) {
-    if (darkerLuminance >= 0.2) {
+    if (darkerLuminance >= lowerLightnessThreshold) {
       darkerLuminance += deltaChange;
       darkerShades.push(darken(deltaChange * (darkerShades.length + 1), color));
     }
 
-    if (lighterLuminance <= 0.8) {
+    if (lighterLuminance <= higherLightnessThreshold) {
       lighterLuminance += deltaChange;
       lighterShades.unshift(
         lighten(deltaChange * (lighterShades.length + 1), color)
@@ -40,12 +44,12 @@ export function computeShades(
 
   const fillerDarkerShades = maxShades - darkerShades.length;
   for (let i = 0; i < fillerDarkerShades; i++) {
-    darkerShades.push('rgb(0, 0, 0, 0)');
+    darkerShades.push(transparentColor);
   }
 
   const fillerLighterShades = maxShades - lighterShades.length;
   for (let i = 0; i < fillerLighterShades; i++) {
-    lighterShades.unshift('rgb(0, 0, 0, 0)');
+    lighterShades.unshift(transparentColor);
   }
 
   return { darkerShades, lighterShades };
