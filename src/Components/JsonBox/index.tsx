7d04ec4db22f20f29393f044a2b83974464ffa15
AXwain <5733365+axwain@users.noreply.github.com>
2023-03-26 19:23:58 -0500

    Implemented JsonBox component to display Json Color definition

---
+++ src/Components/JsonBox/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+type Props = {
+  jsonString: string;
+  onCopy: () => void;
+  onPaste: () => void;
+};
+
+export function JsonBox({ jsonString, onCopy, onPaste }: Props) {
+  return (
+    <>
+      <textarea className="json-box" value={jsonString} readOnly />
+      <div className="horizontal-button-group">
+        <button onClick={onCopy}>Copy</button>
+        <button onClick={onPaste}>Paste</button>
+      </div>
+    </>
+  );
+}
