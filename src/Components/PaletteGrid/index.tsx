2e1f94ba525384a87674ad2845afa94f4ede63bc
AXwain <5733365+axwain@users.noreply.github.com>
2023-04-02 11:34:46 -0500

    Refactored Grid Color preview components into a single component

---
+++ src/Components/PaletteGrid/index.tsx
@@ -0,0 +1,22 @@
+import { useKeyIds } from '../../hooks';
+import { ColorPreview } from '../ColorPreview';
+import './styles.css';
+
+type Props = {
+  colors: string[] | readonly string[];
+  columnCount: number;
+};
+
+export function PaletteGrid({ colors, columnCount }: Props) {
+  const keyIds = useKeyIds(colors.length);
+  return (
+    <div
+      className="palette-grid"
+      style={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)` }}
+    >
+      {colors.map((color, index) => (
+        <ColorPreview key={keyIds[index]} color={color} />
+      ))}
+    </div>
+  );
+}
