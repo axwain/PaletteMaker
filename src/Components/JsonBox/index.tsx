26b9b9e1aeeb406b95cdfc7ad29fc6b8f4cc3673
AXwain <5733365+axwain@users.noreply.github.com>
2023-04-02 20:34:09 -0500

    Make JsonBox loading work for Firefox.
    Removed auto copy of values

--- src/Components/JsonBox/index.tsx
+++ src/Components/JsonBox/index.tsx
@@ -2,17 +2,22 @@ import './styles.css';
 
 type Props = {
   jsonString: string;
+  onChange: (value: string) => void;
   onCopy: () => void;
-  onPaste: () => void;
+  onLoad: () => void;
 };
 
-export function JsonBox({ jsonString, onCopy, onPaste }: Props) {
+export function JsonBox({ jsonString, onChange, onCopy, onLoad }: Props) {
   return (
     <div>
-      <textarea className="json-box" value={jsonString} readOnly />
+      <textarea
+        className="json-box"
+        value={jsonString}
+        onChange={(e) => onChange(e.target.value)}
+      />
       <div className="horizontal-button-group">
         <button onClick={onCopy}>Copy</button>
-        <button onClick={onPaste}>Paste</button>
+        <button onClick={onLoad}>Load</button>
       </div>
     </div>
   );
